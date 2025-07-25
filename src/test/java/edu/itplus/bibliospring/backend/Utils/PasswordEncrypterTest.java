9e85f1bb0c9acfb156f9373799073fa6e5a67840
Laci <152368685+Sempailbk@users.noreply.github.com>
2025-07-25 10:23:14 +0300

    commit

---
+++ src/test/java/edu/itplus/bibliospring/backend/Utils/PasswordEncrypterTest.java
@@ -0,0 +1,12 @@
+package edu.itplus.bibliospring.backend.Utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordEncrypterTest {
+
+    @Test
+    void hashPassword() {
+    }
+}
